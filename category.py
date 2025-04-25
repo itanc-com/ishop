85bb323c60a0f99a5b73e8ee32aeac9fbad0eddd
Ali Bagheri <khodealib@gmail.com>
2025-04-25 14:34:31 +0330

    Make parent_id nullable in Category model

--- category.py
+++ category.py
@@ -8,7 +8,7 @@ class Category(Base):
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, index=True)
-    parent_id = Column(Integer, ForeignKey("categories.id"), index=True)
+    parent_id = Column(Integer, ForeignKey("categories.id"), index=True, nullable=True)
     title = Column(String(255), index=True)
     date_created = Column(DateTime, default=func.now())
     date_modified = Column(DateTime, default=func.now(), onupdate=func.now())
