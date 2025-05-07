17e40d708a50c244d94250b157d80e6c6773e7a8
Babak Jahangiri <babakjahangiri@gmail.com>
2025-05-07 01:22:56 +0100

    auth routers added

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.db.base import Base, engine
 from app.modules.category.models import Category  # noqa
 from app.modules.product.models import Product  # noqa
 from app.modules.product.routers import router as products_router
+from app.modules.auth.routers import router as auth_router
 from app.modules.user.models import User  # noqa
 
 app = FastAPI(
@@ -21,3 +22,4 @@ async def root() -> dict:
 
 
 app.include_router(products_router)
+app.include_router(auth_router)
