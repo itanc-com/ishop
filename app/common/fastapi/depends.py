f720227f22177b00f713ebd10709cfcbcdcdab69
Babak Jahangiri <babakjaangiri@gmail.com>
2025-05-22 02:13:15 +0100

    method added to get product_repository

--- app/common/fastapi/depends.py
+++ app/common/fastapi/depends.py
@@ -2,6 +2,8 @@ from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_db_session
+from app.modules.product.repository import ProductRepository
+from app.modules.product.repository_interface import ProductRepositoryInterface
 from app.modules.user.repository import UserRepository
 from app.modules.user.repository_interface import UserRepositoryInterface
 
@@ -9,3 +11,5 @@ from app.modules.user.repository_interface import UserRepositoryInterface
 def get_user_repository(db_session: AsyncSession = Depends(get_db_session)) -> UserRepositoryInterface:
     return UserRepository(db_session)
 
+def get_product_repository(db_session: AsyncSession = Depends(get_db_session)) -> ProductRepositoryInterface:
+    return ProductRepository(db_session)
