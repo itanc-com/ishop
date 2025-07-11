a4db210fafe83e4c4d86230d44636a4ba31701ce
Ali Bagheri <khodealib@gmail.com>
2025-07-11 11:57:46 +0330

    feat: implement category creation use case

---
+++ app/modules/category/usecases/category_create.py
@@ -0,0 +1,17 @@
+from app.common.exceptions.app_exceptions import DatabaseOperationException
+from app.modules.category.models import Category
+from app.modules.category.repository_interface import CategoryRepositoryInterface
+from app.modules.category.schemas import CategoryCreate, CategoryRead
+
+
+class CategoryCreateUsecase:
+    def __init__(self, category_repository: CategoryRepositoryInterface):
+        self.category_repository = category_repository
+
+    async def execute(self, category_create: CategoryCreate) -> CategoryRead | None:
+        category = Category(**category_create.model_dump())
+        try:
+            category = await self.category_repository.create(category)
+        except Exception as e:
+            raise DatabaseOperationException("create", str(e), data={"category": category})
+        return CategoryRead.model_validate(category)
