29b6e3b9980800b7c5a223395a54478a49ca736a
Ali Bagheri <khodealib@gmail.com>
2025-07-12 20:33:32 +0330

    Add category retrieval functionality
    - Implemented a new endpoint to retrieve a category by its ID in category_retrieve.py.
    - Created a use case class, CategoryGetById, to handle the logic of fetching a category from the repository and managing exceptions.
    - Added appropriate response documentation for successful and error cases.

---
+++ app/modules/category/routers/category_retrieve.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from app.common.http_response.doc_reponses import ResponseErrorDoc, ResponseSuccessDoc
+from app.modules.category.depends import get_category_repository
+from app.modules.category.repository_interface import CategoryRepositoryInterface
+from app.modules.category.routers import router
+from app.modules.category.schemas import CategoryRead
+from app.modules.category.usecases.category_get_by_id import CategoryGetById
+
+
+@router.get(
+    "/{category_id}",
+    response_model=CategoryRead,
+    responses={
+        **ResponseSuccessDoc.HTTP_200_OK("Category retrieved successfully", CategoryRead),
+        **ResponseErrorDoc.HTTP_404_NOT_FOUND("Category not found"),
+    },
+)
+async def get_category_by_id(
+    category_id: int, category_repository: Annotated[CategoryRepositoryInterface, Depends(get_category_repository)]
+) -> CategoryRead:
+    category_read = await CategoryGetById(category_repository).execute(category_id)
+    return category_read
---
+++ app/modules/category/usecases/category_get_by_id.py
@@ -0,0 +1,17 @@
+from app.common.exceptions.app_exceptions import EntityNotFoundException
+from app.modules.category.repository_interface import CategoryRepositoryInterface
+from app.modules.category.schemas import CategoryRead
+
+
+class CategoryGetById:
+    def __init__(self, category_repository: CategoryRepositoryInterface):
+        self.category_repository = category_repository
+
+    async def execute(self, category_id: int) -> CategoryRead:
+        category = await self.category_repository.get_by_id(category_id)
+        if category is None:
+            raise EntityNotFoundException(
+                data={"category_id": category_id},
+                message="Category not found",
+            )
+        return CategoryRead.model_validate(category)
