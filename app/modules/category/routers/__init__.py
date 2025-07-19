a13e15cf23031b94b3f89866e263bc18ec364a96
Ali Bagheri <khodealib@gmail.com>
2025-07-20 00:40:57 +0330

    feat(Category): Add category listing functionality
    - Introduced a new endpoint to list categories in category_list.py.
    - Created a use case class, CategoryList, to handle the logic of fetching categories based on parent_id.
    - Updated the router to include the new category_list endpoint and renamed the category retrieval function for clarity.

--- app/modules/category/routers/__init__.py
+++ app/modules/category/routers/__init__.py
@@ -7,4 +7,5 @@ router = APIRouter(
 )
 
 from .category_create import category_create  # noqa: F401, E402
-from .category_retrieve import get_category_by_id  # noqa: F401, E402
+from .category_list import category_list  # noqa: F401, E402
+from .category_retrieve import category_get_by_id  # noqa: F401, E402
