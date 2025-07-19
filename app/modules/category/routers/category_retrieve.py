1c0f0d8baecd4319543a310d622d7502ec7150ae
Ali Bagheri <khodealib@gmail.com>
2025-07-20 00:39:56 +0330

    refactor(category): enhance category retrieval response with success message and status code

--- app/modules/category/routers/category_retrieve.py
+++ app/modules/category/routers/category_retrieve.py
@@ -1,8 +1,10 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, status
+from fastapi.requests import Request
 
 from app.common.http_response.doc_reponses import ResponseErrorDoc, ResponseSuccessDoc
+from app.common.http_response.success_result import SuccessResult
 from app.modules.category.depends import get_category_repository
 from app.modules.category.repository_interface import CategoryRepositoryInterface
 from app.modules.category.routers import router
@@ -18,11 +20,18 @@ from app.modules.category.usecases.category_get_by_id import CategoryGetById
         **ResponseErrorDoc.HTTP_404_NOT_FOUND("Category not found"),
     },
 )
-async def get_category_by_id(
-    category_id: int, category_repository: Annotated[CategoryRepositoryInterface, Depends(get_category_repository)]
+async def category_get_by_id(
+    request: Request,
+    category_id: int,
+    category_repository: Annotated[CategoryRepositoryInterface, Depends(get_category_repository)],
 ) -> CategoryRead:
     category_by_id_usecase = CategoryGetById(category_repository)
 
     category_read = await category_by_id_usecase.execute(category_id)
 
-    return category_read
+    return SuccessResult[CategoryRead](
+        code=status.HTTP_200_OK,
+        message="Category retrieved successfully",
+        status_code=status.HTTP_200_OK,
+        data=category_read,
+    ).to_json_response(request)
