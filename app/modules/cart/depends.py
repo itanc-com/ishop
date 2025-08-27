b92a1ef5845eedd364e85c554d185edb12834f06
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-08-28 02:26:48 +0330

    fix(cart): provide proper CartItemRepository instance for dependency injection

--- app/modules/cart/depends.py
+++ app/modules/cart/depends.py
@@ -3,8 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_db_session
 
+from .repository import CartItemRepository
 from .repository_interface import CartItemRepositoryInterface
 
 
 def get_cartitem_repository(db_session: AsyncSession = Depends(get_db_session)) -> CartItemRepositoryInterface:
-    return CartItemRepositoryInterface(db_session)
+    return CartItemRepository(db_session)
