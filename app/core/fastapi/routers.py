93dde683d1bc976491bd31d338c4e9be326cada0
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-08-28 02:32:05 +0330

    chore(cart): include Cart router in main FastAPI app

--- app/core/fastapi/routers.py
+++ app/core/fastapi/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.modules.auth.routers import router as auth_router
+from app.modules.cart.routers import router as cart_router
 from app.modules.category.routers import router as category_router
 from app.modules.product.routers import router as products_router
 from app.modules.user.routers import router as user_router
@@ -10,6 +11,7 @@ router_v1 = APIRouter(prefix="/v1")
 """Include to router all api rest routes with version prefix"""
 
 router_v1.include_router(auth_router)
+router_v1.include_router(cart_router)
 router_v1.include_router(category_router)
 router_v1.include_router(products_router)
 router_v1.include_router(user_router)
