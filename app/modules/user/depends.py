6c71fcf4b2ef7d211460fe823b88d400e5fad1cf
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-10 03:16:59 +0330

    refactor(user): use concrete UserRepository in dependency instead of interface

--- app/modules/user/depends.py
+++ app/modules/user/depends.py
@@ -3,8 +3,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_db_session
 
+from .repository import UserRepository
 from .repository_interface import UserRepositoryInterface
 
 
-def get_user_repository(db_session: AsyncSession = Depends(get_db_session)) -> UserRepositoryInterface:
-    return UserRepositoryInterface(db_session)
+def get_user_repository(
+    db_session: AsyncSession = Depends(get_db_session)
+) -> UserRepositoryInterface:
+    return UserRepository(db_session)
