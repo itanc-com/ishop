467b0ecff4a8480217a51a8ba76db4c65acca6be
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-10 03:21:57 +0330

    fix(tests/e2e): update server status test to use lowercase 'status' key

--- tests/e2e/test_cases/test_status.py
+++ tests/e2e/test_cases/test_status.py
@@ -1,13 +1,8 @@
-# from fixtures import api_request_context
-from .config import SERVER_URL
-
+from tests.e2e.config import SERVER_URL
 
 def test_get_server_status(api_request_context):
     response = api_request_context.get(f"{SERVER_URL}/")
     assert response.ok
     data = response.json()
-    assert "Status" in data
-    assert data["Status"] == "OK"
-
-
-# pytest tests/e2e -v -s
+    assert "status" in data
+    assert data["status"] == "OK"
