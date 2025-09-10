a0a534e39f5b043ad04686521a4218ebe43ff4e5
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-10 23:42:23 +0330

    test: add sample users with normal and weak passwords

---
+++ tests/e2e/data/users.py
@@ -0,0 +1,10 @@
+USERS = {
+    "normal": {
+        "email": "user@example.com",
+        "password": "UserPass123!",
+    },
+    "weak_password": {
+        "email": "weak@example.com",
+        "password": "123",
+    },
+}
