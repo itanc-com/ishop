9114408b9df6016d444ca67b1a18cb3aaa1ebb91
Babak Jahangiri <babakjahangiri@gmail.com>
2025-11-04 02:08:08 +0000

    test(e2e): add global_setup with clear_database and logger

--- tests/e2e/fixtures/fxt_base.py
+++ tests/e2e/fixtures/fxt_base.py
@@ -17,7 +17,7 @@ def api_request_context() -> Generator[APIRequestContext, Any, None]:
 
 @pytest.fixture(autouse=True, scope="session")
 def logger() -> Logger:
-    return get_logger(service=ServiceName.TEST_SERVICE)
+    return get_logger(service=ServiceName.E2E_TEST_SERVICE)
 
 
 # login and get user tokens
