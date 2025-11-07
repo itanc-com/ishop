873bb5a172fe63245e581c17da41b869a3040f39
Babak Jahangiri <babakjahangiri@gmail.com>
2025-11-07 23:25:57 +0000

    token type added

--- app/modules/auth/schemas.py
+++ app/modules/auth/schemas.py
@@ -66,6 +66,7 @@ class JWTPayload(BaseModel):
 class TokenResponse(BaseModel):
     access_token: str
     refresh_token: str
+    token_type: str = "bearer"
     access_token_expire_in: int
     refresh_token_expire_in: int
 
