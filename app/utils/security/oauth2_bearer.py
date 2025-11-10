197a8afae898247258ad3482e4a71cc32406c784
Babak Jahangiri <babakjahangiri@gmail.com>
2025-11-10 23:31:50 +0000

    make all 401 responses use custom ErrorResponseWrapper format instead of FastAPIs default: Not authenticated

--- app/utils/security/oauth2_bearer.py
+++ app/utils/security/oauth2_bearer.py
@@ -1,7 +1,19 @@
+from fastapi import HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer
 
+from app.common.exceptions.app_exceptions import AuthenticationException
+from app.common.http_response.error_response import ErrorCodes
 from app.core.config.constants import AUTH_TOKEN_SWAGGER
 
-oauth2_bearer = OAuth2PasswordBearer(
+
+class CustomOAuth2PasswordBearer(OAuth2PasswordBearer):
+    async def __call__(self, request: Request) -> str:
+        try:
+            return await super().__call__(request)
+        except HTTPException as e:
+            raise AuthenticationException(code=ErrorCodes.UNAUTHORIZED, message="Not authenticated", data=None) from e
+
+
+oauth2_bearer = CustomOAuth2PasswordBearer(
     tokenUrl=AUTH_TOKEN_SWAGGER,
 )
